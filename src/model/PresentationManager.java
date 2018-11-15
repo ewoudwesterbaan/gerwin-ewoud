f7951a309e9db1770ac8e628b86b10f3fbb3e011
gerwinvandijken <gerwin.vandijken@inholland.nl>
2018-11-15 20:42:20 +0100

    Update PresentationManager.java

--- src/model/PresentationManager.java
+++ src/model/PresentationManager.java
@@ -1,54 +1,129 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import factory.ReaderFactory;
 import reader.Reader;
 
 public class PresentationManager implements Presenter {
-	
+
+	// Java kent ook (class) java.util.Observable en (interface) Observer
+	// de PresentationManager implementeert de Observer pattern nu 'zelf'
+	// (zo laten?)
+
 	private List<Presentation> presentations;
+	private List<PresenterObserver> observers;
+	private Presentation currentPresentation;
+	private int currentPresentationIndex; // wel zo handig...
+
+	public PresentationManager() {
+		presentations = new ArrayList<Presentation>();
+		observers = new ArrayList<PresenterObserver>();
+
+		// initially, no presentation selected
+		reset();
+	}
+
+	private void reset() {
+		currentPresentation = null;
+		currentPresentationIndex = -1;
+
+		// state has changed, notify all observers
+		notifyAllObservers();
+	}
+	
+	@Override
+	public int getSize() {
+		return presentations.size();
+	}
+	
+	@Override
+	public void append(Presentation presentation) {
+		presentations.add(presentation);
+	}
 
 	@Override
+	public Presentation getCurrentPresentation() {
+		return currentPresentation;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (presentations.size() == 0)
+			return false;
+		return (currentPresentationIndex < (presentations.size() - 1));
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		if (presentations.size() == 0)
+			return false;
+		return (currentPresentationIndex > 0);
+	}
+
+	@Override
+	// assumption: zero-based indexing
 	public void selectPresentation(int number) {
-		// TODO Auto-generated method stub
-		
+		if ((number < 0) || (number >= presentations.size()))
+			return;
+
+		currentPresentation = presentations.get(number);
+		currentPresentationIndex = number;
+
+		// restart presentation at first slide
+		currentPresentation.selectSlide(0);
+
+		// state has changed, notify all observers
+		notifyAllObservers();
 	}
 
 	@Override
 	public void next() {
-		// TODO Auto-generated method stub
-		
+		selectPresentation(currentPresentationIndex + 1);
 	}
 
 	@Override
 	public void previous() {
-		// TODO Auto-generated method stub
-		
+		selectPresentation(currentPresentationIndex - 1);
 	}
 
 	@Override
 	public void attach(PresenterObserver presenterObserver) {
-		// TODO Auto-generated method stub
-		
+		// add observer to observer list
+		if (!observers.contains(presenterObserver))
+			observers.add(presenterObserver);
 	}
 
 	@Override
 	public void detach(PresenterObserver presenterObserver) {
-		// TODO Auto-generated method stub
-		
+		// remove observer from observer list
+		if (observers.contains(presenterObserver))
+			observers.remove(presenterObserver);
+	}
+
+	private void notifyAllObservers() {
+		// update all presenter observers
+		for (PresenterObserver observer : observers) {
+			observer.update(currentPresentation);
+		}
 	}
 
 	@Override
 	public void loadFile(String fileName) {
 		Reader reader = ReaderFactory.getInstance().getReader(fileName);
 		presentations = reader.load();
+
+		// after loading of presentations, the first one is automatically selected
+		if (presentations.size() > 0)
+			selectPresentation(0);
+		else
+			reset();
 	}
 
 	@Override
 	public void saveFile(String fileName) {
 		// TODO Auto-generated method stub
-		
+		// ...
 	}
-
 }
