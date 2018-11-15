977b80e631c6b96c0d145ab2ee642c83ebc33bfe
gerwinvandijken <gerwin.vandijken@inholland.nl>
2018-11-15 20:41:47 +0100

    Update JabberPoint.java

--- src/JabberPoint.java
+++ src/JabberPoint.java
@@ -2,17 +2,23 @@ import javax.swing.JOptionPane;
 
 import model.Accessor;
 import model.Presentation;
+import model.PresentationManager;
+import model.Presenter;
 import model.Style;
 import model.XMLAccessor;
 import view.SlideViewerFrame;
 
 import java.io.IOException;
 
-/** JabberPoint Main Programma
- * <p>This program is distributed under the terms of the accompanying
- * COPYRIGHT.txt file (which is NOT the GNU General Public License).
- * Please read it. Your use of the software constitutes acceptance
- * of the terms in the COPYRIGHT.txt file.</p>
+/**
+ * JabberPoint Main Programma
+ * <p>
+ * This program is distributed under the terms of the accompanying COPYRIGHT.txt
+ * file (which is NOT the GNU General Public License). Please read it. Your use
+ * of the software constitutes acceptance of the terms in the COPYRIGHT.txt
+ * file.
+ * </p>
+ * 
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -29,20 +35,25 @@ public class JabberPoint {
 
 	/** Het Main Programma */
 	public static void main(String argv[]) {
+
+		// Presentation presentation = new Presentation();
+		Presenter presenter = new PresentationManager();
+		presenter.loadFile("../test_slidesequences.xml");
+
+		new SlideViewerFrame(JABVERSION, presenter);
 		
-		Presentation presentation = new Presentation();
-		new SlideViewerFrame(JABVERSION, presentation);
-		try {
+		presenter.selectPresentation(0);
+		
+		// stuk hieronder kan weg?
+		/*try {
 			if (argv.length == 0) { // een demo presentatie
 				Accessor.getDemoAccessor().loadFile(presentation, "");
 			} else {
 				new XMLAccessor().loadFile(presentation, argv[0]);
 			}
-			presentation.setSlideNumber(0);
+			presentation.selectSlide(0);
 		} catch (IOException ex) {
-			JOptionPane.showMessageDialog(null,
-					IOERR + ex, JABERR,
-					JOptionPane.ERROR_MESSAGE);
-		}
+			JOptionPane.showMessageDialog(null, IOERR + ex, JABERR, JOptionPane.ERROR_MESSAGE);
+		}*/
 	}
 }
