68bce0141ae9d1fe563ba5931515e7db8031644f
gerwinvandijken <gerwin.vandijken@inholland.nl>
2018-11-15 20:42:25 +0100

    Update Presenter.java

--- src/model/Presenter.java
+++ src/model/Presenter.java
@@ -2,9 +2,14 @@ package model;
 
 public interface Presenter {
 
-	void selectPresentation(int number);
+	int getSize();
+	void append(Presentation presentation);
+	boolean hasNext();
+	boolean hasPrevious();
 	void next();
 	void previous();
+	void selectPresentation(int number);
+	Presentation getCurrentPresentation();
 	void attach(PresenterObserver presenterObserver);
 	void detach(PresenterObserver presenterObserver);
 	void loadFile(String fileName);
