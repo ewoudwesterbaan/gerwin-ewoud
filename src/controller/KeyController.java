a2f232bec3a39ddaeb34e08609defd5c7a627b0a
gerwinvandijken <gerwin.vandijken@inholland.nl>
2018-11-15 20:41:24 +0100

    Update KeyController.java

--- src/controller/KeyController.java
+++ src/controller/KeyController.java
@@ -1,11 +1,18 @@
 package controller;
+
 import java.awt.event.KeyEvent;
 
 import model.Presentation;
+import model.Presenter;
+import model.PresenterObserver;
 
 import java.awt.event.KeyAdapter;
 
-/** <p>This is the KeyController (KeyListener)</p>
+/**
+ * <p>
+ * This is the KeyController (KeyListener)
+ * </p>
+ * 
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -13,34 +20,40 @@ import java.awt.event.KeyAdapter;
  * @version 1.4 2007/07/16 Sylvia Stuurman
  * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
-*/
+ */
 
 public class KeyController extends KeyAdapter {
 	private Presentation presentation; // Er worden commando's gegeven aan de presentatie
 
-	public KeyController(Presentation p) {
-		presentation = p;
+	public KeyController(Presentation presentation) {
+		this.presentation = presentation;
 	}
 
 	public void keyPressed(KeyEvent keyEvent) {
-		switch(keyEvent.getKeyCode()) {
-			case KeyEvent.VK_PAGE_DOWN:
-			case KeyEvent.VK_DOWN:
-			case KeyEvent.VK_ENTER:
-			case '+':
-				presentation.nextSlide();
-				break;
-			case KeyEvent.VK_PAGE_UP:
-			case KeyEvent.VK_UP:
-			case '-':
-				presentation.prevSlide();
-				break;
-			case 'q':
-			case 'Q':
-				System.exit(0);
-				break; // wordt nooit bereikt als het goed is
-			default:
-				break;
+		switch (keyEvent.getKeyCode()) {
+		case KeyEvent.VK_PAGE_DOWN:
+		case KeyEvent.VK_DOWN:
+		case KeyEvent.VK_ENTER:
+		case '+':
+			if (presentation != null)
+				presentation.next(); // nextSlide();
+			break;
+		case KeyEvent.VK_PAGE_UP:
+		case KeyEvent.VK_UP:
+		case '-':
+			if (presentation != null)
+				presentation.previous(); // prevSlide();
+			break;
+		case 'q':
+		case 'Q':
+			System.exit(0);
+			break; // wordt nooit bereikt als het goed is
+		default:
+			break;
 		}
+	}
+
+	public void switchPresentation(Presentation presentation) {
+		this.presentation = presentation;
 	}
 }
