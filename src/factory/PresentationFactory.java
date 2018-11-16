94d32a188e6049b024ae2e31dd7f1ea9085439bd
Ewoud Westerbaan <ewoud.westerbaan@topbi.nl>
2018-11-16 14:51:18 +0100

    Add comments

--- src/factory/PresentationFactory.java
+++ src/factory/PresentationFactory.java
@@ -4,7 +4,7 @@ import model.Presentation;
 import model.SlideSequence;
 
 /**
- * 
+ * Verantwoordelijk voor het aanmaken van {@link Presentation} objecten.
  * @author Gerwin van Dijken
  * @author Ewoud Westerbaan
  * @since 2.0
@@ -15,6 +15,10 @@ public class PresentationFactory {
 	
 	private static PresentationFactory instance;
 	
+	/**
+	 * 
+	 * @return De {@link PresentationFactory} om {@link Presentation} objecten mee te maken.
+	 */
 	public static PresentationFactory getInstance() {
 		if (instance == null) {
 			instance = new PresentationFactory();
@@ -22,14 +26,23 @@ public class PresentationFactory {
 		return instance;
 	}
 	
+	/**
+	 * Constructor is {@code private} omdat alleen deze klasse een object van zichzelf mag maken.
+	 */
 	private PresentationFactory() {
 		
 	}
 	
-	// concatenate sequence title with presentation title
-	public Presentation getPresentation(String showTitle, String sequenceTitle) {
-		String title = showTitle + " - " + sequenceTitle;
-		Presentation presentation = new SlideSequence(title);
+	/**
+	 * Geeft een implementatie terug van {@link Presentation}.
+	 * @param showTitle Title van de presentatie.
+	 * @param subTitle Subtitel van de presentie (bijvoorbeeld de titel van een aparte volgorde).
+	 * @return Presentation object.
+	 */
+	public Presentation getPresentation(String showTitle, String subTitle) {
+		// concatenate sequence title with presentation title
+		String title = showTitle + " - " + subTitle;
+		Presentation presentation = new SlideSequence(title); // De enige veriant die tot nu toe is geïmplementeerd.
 		return presentation;
 	}
 
